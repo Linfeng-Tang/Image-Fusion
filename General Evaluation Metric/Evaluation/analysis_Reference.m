de968ea7b0795ff708785292f950ee65ea5fdbd7
Linfeng Tang <59408033+Linfeng-Tang@users.noreply.github.com>
2023-03-03 11:40:54 +0800

    Update analysis_Reference.m

--- General Evaluation Metric/Evaluation/analysis_Reference.m
+++ General Evaluation Metric/Evaluation/analysis_Reference.m
@@ -1,37 +1,40 @@
-function [EN, SF, SD, PSNR, MSE, MI, VIF, AG, CC, SCD, Qabf, Nabf, SSIM, MS_SSIM, FMI_pixel, FMI_dct, FMI_w] = analysis_Reference(image_f,image_ir,image_vis, easy)
+function [EN, SF, SD, PSNR, MSE, MI, VIF, AG, CC, SCD, Qabf, Nabf, SSIM, MS_SSIM, FMI_pixel, FMI_dct, FMI_w] = analysis_Reference(img_f,img1,img2, easy)
+    % 输入的img_f, img1, and img2 均是Uint8型，
+    % 计算EN和MI是需要使用uint8的数据，其他指标均转换成float型的数据，
+    % 计算MSE和PSNR时数据归一化到[0-1]，其他指标计算时数据的范围均是[0-255]
     grey_level = 256;
-    [s1,s2] = size(image_ir);
+    [s1,s2] = size(img1);
+    img1_int = img1;
+    img2_int = img2;
+    img_f_int = img_f;
+    img1_float = im2double(img1) * 255.0;
+    img2_float = im2double(img2) * 255.0;
+    img_f_float = im2double(img_f) * 255.0;
     imgSeq = zeros(s1, s2, 2);
-    imgSeq(:, :, 1) = image_ir;
-    imgSeq(:, :, 2) = image_vis;
-
-    image1 = im2double(image_ir);
-    image2 = im2double(image_vis);
-    image_fused = im2double(image_f);
+    imgSeq(:, :, 1) = img1_float;
+    imgSeq(:, :, 2) = img2_float;
     if easy ==1
         %EN
-        EN = entropy(image_f);
-
-        %SF
-        SF = SF_evaluation(image_fused);
-        %SD
-        SD = SD_evaluation(image_f);
-        %PSNR
-        PSNR = PSNR_evaluation(image1,image2,image_fused);
-        %MSE
-        MSE = MSE_evaluation(image1,image2,image_fused);
+        EN = entropy(img_f_int);        
         %MI
-        MI = MI_evaluation(image_ir,image_vis,image_f, grey_level);
+        MI = MI_evaluation(img1_int, img2_int, img_f_int, grey_level);        
+        %PSNR
+        PSNR = PSNR_evaluation(img1_float, img2_float, img_f_float);
+        %MSE
+        MSE = MSE_evaluation(img1_float, img2_float, img_f_float);
+        %SF
+        SF = SF_evaluation(img_f_float);
+        %SD
+        SD = SD_evaluation(img_f_float);
         %VIF
-        VIF = vifp_mscale(image_ir, image_f) + vifp_mscale(image_vis, image_f);
+        VIF = vifp_mscale(img1_float, img_f_float) + vifp_mscale(img2_float, img_f_float);
         %AG
-        AG = AG_evaluation(image_f);
+        AG = AG_evaluation(img_f_float);
         %CC
-        CC = CC_evaluation(image1, image2, image_fused);
+        CC = CC_evaluation(img1_float, img2_float, img_f_float);
         %SCD
-        SCD = analysis_SCD(image1, image2, image_fused);
-        %Qabf
-        Qabf = analysis_Qabf(image1, image2, image_fused);
+        SCD = analysis_SCD(img1_float, img2_float, img_f_float);        %Qabf
+        Qabf = analysis_Qabf(img1_float, img2_float, img_f_float);
         Nabf = 1;
         SSIM = 1;
         MS_SSIM = 1;
@@ -63,31 +66,17 @@ function [EN, SF, SD, PSNR, MSE, MI, VIF, AG, CC, SCD, Qabf, Nabf, SSIM, MS_SSIM
         %Qabf
         Qabf = 1;
         % %Nabf
-        Nabf = analysis_nabf(image_fused, image1, image2);
+        Nabf = analysis_nabf(img1_float, img2_float, img_f_float);
         % SSIM_a
-        % SSIM1 = ssim(image_fused,image1);
-        % SSIM2 = ssim(image_fused,image2);
-        SSIM = mef_ssim(imgSeq, image_f);
+        SSIM1 = ssim(img_f_float,img1_float);
+        SSIM2 = ssim(img_f_float,img2_float);
+        SSIM = 0.5 * SSIM1 + 0.5 * SSIM2;
+%         SSIM = mef_ssim(imgSeq, img_f);
         %MS_SSIM
-        [MS_SSIM,t1,t2]= analysis_ms_ssim(imgSeq, image_f);
+        [MS_SSIM,t1,t2]= analysis_ms_ssim(imgSeq, img_f_float);
         %FMI
-        FMI_pixel = analysis_fmi(image1,image2,image_fused);
-        FMI_dct = analysis_fmi(image1,image2,image_fused,'dct');
-        FMI_w = analysis_fmi(image1,image2,image_fused,'wavelet');
-%         Nabf = 1;
-%         SSIM = 1;
-%         MS_SSIM = 1;
-%         FMI_pixel = 1;
-%         FMI_dct = 1;
-%         FMI_w = 1; 
+        FMI_pixel = analysis_fmi(img1_float, img2_float, img_f_float);
+        FMI_dct = analysis_fmi(img1_float, img2_float, img_f_float,'dct');
+        FMI_w = analysis_fmi(img1_float, img2_float, img_f_float,'wavelet');
     end
-
-
 end
-
-
-
-
-
-
-
