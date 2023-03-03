19332f3adaa3e200ac2f7ce0fb2e14755abd0c60
Linfeng Tang <59408033+Linfeng-Tang@users.noreply.github.com>
2023-03-03 11:41:15 +0800

    Update PSNR_evaluation.m

--- General Evaluation Metric/Evaluation/PSNR_evaluation.m
+++ General Evaluation Metric/Evaluation/PSNR_evaluation.m
@@ -1,7 +1,10 @@
 function PSNR = PSNR_evaluation(A,B,F)
-[m,n]=size(F);
-MSE_AF=sum(sum((F-A).^2))/(m*n);
-MSE_BF=sum(sum((F-B).^2))/(m*n);
-MSE=0.5*MSE_AF+0.5*MSE_BF; 
-PSNR=20*log10(255/sqrt(MSE));
+  A = A / 255.0;
+  B = B /255.0;
+  F = F / 255.0;
+  [m,n]=size(F);
+  MSE_AF=sum(sum((F-A).^2))/(m*n);
+  MSE_BF=sum(sum((F-B).^2))/(m*n);
+  MSE=0.5*MSE_AF+0.5*MSE_BF; 
+  PSNR=20*log10(255/sqrt(MSE));
 end
