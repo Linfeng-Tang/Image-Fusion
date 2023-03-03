9e6159f4a09ece3d3a1da6f9ca444436b7012c64
Linfeng Tang <59408033+Linfeng-Tang@users.noreply.github.com>
2023-03-03 11:41:40 +0800

    Update MSE_evaluation.m

--- General Evaluation Metric/Evaluation/MSE_evaluation.m
+++ General Evaluation Metric/Evaluation/MSE_evaluation.m
@@ -1,6 +1,9 @@
 function MSE = MSE_evaluation(A,B,F)
-[m,n]=size(F);
-MSE_AF=sum(sum((F-A).^2))/(m*n);
-MSE_BF=sum(sum((F-B).^2))/(m*n);
-MSE=0.5*MSE_AF+0.5*MSE_BF;
+  A = A / 255.0;
+  B = B /255.0;
+  F = F / 255.0;
+  [m,n]=size(F);
+  MSE_AF=sum(sum((F-A).^2))/(m*n);
+  MSE_BF=sum(sum((F-B).^2))/(m*n);
+  MSE=0.5*MSE_AF+0.5*MSE_BF;
 end
